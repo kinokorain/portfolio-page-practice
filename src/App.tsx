cee4180767d0899767973e8f538e6dd3e528529e
Boris <boriskoznev@gmail.com>
2024-08-29 15:07:32 +0300

    fix image url's

--- src/App.tsx
+++ src/App.tsx
@@ -11,11 +11,12 @@ function App() {
         <>
             <div className="container">
                 <Intro />
+                {/* <img src={require('/images/image-name.png')} alt="" /> */}
                 <FeaturedWork />
                 <AboutMe />
                 <div className="image full-width">
                     <img
-                        src="https://s3-alpha-sig.figma.com/img/cea9/165c/ff76f41aa421494e6b5df7890d84b171?Expires=1711324800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Wg~O-RA-CKTBN~V2f4zdOLjxn0g7uW82C6zkQ2fiBye6SLP8vLjYP0W4tRVB581veRlGeiHvaUpoApTtSpHwb~l5Ru35Obyka7-ceA9~hXUV3ivNodwVXYYf~JyGDVgo3UIKVHQIda5UWERTck2rtdScBmuws~xDrbAJvj~LZK6LpTOWYSGwqXJtY0MNJkQwE6flpKlB5PaQi1kzjeurN82BSsGHeZob0VhlJ48QfZO2ZfsPeZ45LHsi-lr-Q-QciFoTK~mr80NauXCYIoxC6gELIZjyU3V9TGCaZ3Lp5G0bjgD84NFmHTLDJPqE0soz-B5IrumdUZ8yjAeuKERxlg__"
+                        src="./assets/images/dude.jfif"
                         alt=""
                     />
                 </div>
